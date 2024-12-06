16e93698ada455da20157dc868fad7e13a24c710
Thomas Arnold <ybn4aq@virginia.edu>
2024-12-06 14:38:16 -0500

    day 4 part 1 complete

--- day5/part1.py
+++ day5/part1.py
@@ -1,8 +1,8 @@
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-file_path1 = os.path.join(current_dir, "test_input1.txt")
-file_path2 = os.path.join(current_dir, "test_input2.txt")
+file_path1 = os.path.join(current_dir, "input1.txt")
+file_path2 = os.path.join(current_dir, "input2.txt")
 f1 = open(file_path1, "r")
 f2 = open(file_path2, "r")
 init1 = f1.readlines()
@@ -41,7 +41,6 @@ for i in page_numbers:
         printed.add(page)
     if valid:
         valid_rules.append(cur_rule)
-print(valid_rules)
 
 # Add up middle values of valid rules
 ret = 0  # return
