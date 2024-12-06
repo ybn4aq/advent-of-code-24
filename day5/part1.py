be226d8f1a3afc0efb081d8fd9f7cf83cd0710b1
Thomas Arnold <ybn4aq@virginia.edu>
2024-12-06 14:37:45 -0500

    day 4 part 1 pass test case

--- day5/part1.py
+++ day5/part1.py
@@ -10,14 +10,10 @@ init2 = f2.readlines()
 input1 = []
 for i in init1:
     input1.append(i.split()[0])
-# print(input1)
 page_numbers = []
 for i in init2:
     page_numbers.append(i.split()[0])
-# print(page_numbers)
-"""
-Need to make dictionary of {rule: [prereq rules]}
-"""
+# Need to make dictionary of {rule: [prereq rules]}
 order = {}
 for i in input1:
     line = i.split("|")
@@ -27,23 +23,30 @@ for i in input1:
         order[rule] = [prereq]
     else:
         order[rule].append(prereq)
-print(order)
-print(page_numbers)
 valid_rules = []
 # Check valid rules
+# 75,97,47,61,53
+# for i in line: if any of prereqs have been printed before, invalid
 for i in page_numbers:
     cur_rule = i.split(",")
     printed = set()
     valid = True
     for j in cur_rule:
         page = int(j)
+        if page in order:
+            for prereq in order[page]:
+                if prereq in printed:
+                    valid = False
+                    break
+        printed.add(page)
     if valid:
         valid_rules.append(cur_rule)
-
+print(valid_rules)
 
 # Add up middle values of valid rules
-
-
-
-
-print(valid_rules)
+ret = 0  # return
+for i in valid_rules:
+    n = len(i)
+    mid = n // 2
+    ret += int(i[mid])
+print(ret)
